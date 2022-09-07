2c706075d832e4c1c240877796baef0b3059ea86
petebrown <15838790+petebrown@users.noreply.github.com>
2022-09-07 13:32:14 -0400

    chromium test

--- update-results.py
+++ update-results.py
@@ -13,7 +13,7 @@ from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
-driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+chrome_service = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
 # chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
