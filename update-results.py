9dd92a1c7c0db20af0c7245cadecd8c2fc294641
Pete Brown <15838790+petebrown@users.noreply.github.com>
2023-02-15 10:57:02 -0500

    Changed Chrome version to 109

--- update-results.py
+++ update-results.py
@@ -22,7 +22,7 @@ options = [
 for option in options:
     chrome_options.add_argument(option)
 
-driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=108)
+driver = uc.Chrome(service=chrome_service, options=chrome_options, version_main=109)
 
 url = 'https://www.11v11.com/teams/tranmere-rovers/tab/matches/'
 
